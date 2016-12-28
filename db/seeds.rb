cd2984f8f1edc1e0a32229b2f1831be7cc2ec9fa
mindziukasss <mindziukasss@gmail.com>
2016-12-28 11:09:57 +0200

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,17 +15,17 @@ AdminUser.create(email: "admin@admin.com",
        phone: "4322386131")
 puts "1 Admin User created"
 
-20.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content Mustache vinyl raw denim pork belly synth normcore ramps ennui offal, post-ironic dreamcatcher woke tbh enamel pin. Chartreuse 90's iPhone roof party, succulents hexagon vegan drinking vinegar iceland lo-fi man braid. Woke schlitz listicle microdosing. Authentic intelligentsia actually, disrupt cold-pressed vaporware cornhole four loko hella umami skateboard vape.", user_id: @user.id, overtime_request: 3.5)
-end
-puts "20 Posts have been created"
-
 
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
+puts "3 Audit_logs have been created"
+
+20.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Mustache vinyl raw denim pork belly synth normcore ramps ennui offal, post-ironic dreamcatcher woke tbh enamel pin. Chartreuse 90's iPhone roof party, succulents hexagon vegan drinking vinegar iceland lo-fi man braid. Woke schlitz listicle microdosing. Authentic intelligentsia actually, disrupt cold-pressed vaporware cornhole four loko hella umami skateboard vape.", user_id: @user.id, overtime_request: 3.5)
+end
+puts "20 Posts have been created"
 
 
-puts "20 Audit_logs have been created"
 
